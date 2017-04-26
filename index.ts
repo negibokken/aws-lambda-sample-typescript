196049df2b91766e53c3456fe647e65467b1c2db
negibokken <negibokken@gmail.com>
2017-04-26 22:27:53 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
-import * as aws from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 import * as request from 'request';
 
 interface IAWSEvent {}
 interface IContext {}
 
-// exports.handler = (event: any, context: any, callback: Function): void => {
+function publish(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const sns: AWS.SNS = new AWS.SNS();
+    let params: any = {
+      Subject: 'test',
+      Message: 'test message',
+    };
+    sns.publish(params);
+
+  });
+}
+
 export function handler(event: any, context: any, callback: Function): void {
   callback(null, 'success');
 };
