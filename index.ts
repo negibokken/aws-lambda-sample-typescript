b706dd0f73c1ce9128f848f4f5bbca65fe20d390
negibokken <negibokken@gmail.com>
2017-04-28 22:27:28 +0900

    WIP

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import AWS = require('aws-sdk');
 import * as request from 'request';
 
-interface IAWSEvent {}
-interface IContext {}
-
 function publish(): Promise<any> {
   return new Promise((resolve, reject) => {
     const sns: AWS.SNS = new AWS.SNS();
@@ -11,12 +8,18 @@ function publish(): Promise<any> {
       Subject: 'test',
       Message: 'test message',
     };
-    sns.publish(params, (err, data) => {
+    sns.publish(params, (err: AWS.AWSError, data: AWS.SNS.PublishResponse) => {
       err ? reject(err) : resolve(data);
     });
   });
 }
 
 export function handler(event: any, context: any, callback: Function): void {
-  callback(null, 'success');
+  publish()
+    .then(() => {
+      callback(null, 'success');
+    })
+    .catch((err: AWS.AWSError) => {
+      callback(err);
+    });
 };
