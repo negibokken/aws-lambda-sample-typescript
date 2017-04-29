2960a9b80d25a0729025b157f80fde36627eb76d
negibokken <negibokken@gmail.com>
2017-04-29 18:15:28 +0900

    Refactoring

--- index.ts
+++ index.ts
@@ -1,25 +1,30 @@
-import AWS = require('aws-sdk');
-import * as request from 'request';
+import AWS = require("aws-sdk");
+import * as request from "request";
+
+interface IName {
+  aaa: string;
+}
 
 function publish(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const sns: AWS.SNS = new AWS.SNS();
-    let params: AWS.SNS.PublishInput = {
-      Subject: 'test',
-      Message: 'test message',
-    };
-    sns.publish(params, (err: AWS.AWSError, data: AWS.SNS.PublishResponse) => {
-      err ? reject(err) : resolve(data);
+    return new Promise((resolve, reject) => {
+        const sns: AWS.SNS = new AWS.SNS();
+        let params: AWS.SNS.PublishInput = {
+            Subject: "test",
+            Message: "test message",
+        };
+        sns.publish(params, (err: AWS.AWSError, data: AWS.SNS.PublishResponse) => {
+            err ? reject(err) : resolve(data);
+        });
+        let a: string[] = ["aaa", "bbb", "ccc"];
     });
-  });
 }
 
 export function handler(event: any, context: any, callback: Function): void {
-  publish()
-    .then(() => {
-      callback(null, 'success');
-    })
-    .catch((err: AWS.AWSError) => {
-      callback(err);
-    });
+    publish()
+        .then(() => {
+            callback(null, "success");
+        })
+        .catch((err: AWS.AWSError) => {
+            callback(err);
+        });
 };
