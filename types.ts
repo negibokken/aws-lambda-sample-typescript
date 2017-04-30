72610f45dc274c08dae1a540b9fef83d8e6c3eeb
negibokken <negibokken@gmail.com>
2017-04-30 16:00:15 +0900

    Add interfaces

---
+++ types.ts
@@ -0,0 +1,49 @@
+interface PinpointLocations {
+   link: string;
+   name: string;
+}
+
+interface Forecast {
+    datalabel: string;
+    telop: string;
+    date: string;
+    temperature: Object;
+    image: Object;
+}
+
+interface Description {
+    tet: string;
+    publicTime: string;
+}
+
+interface Image {
+    width: number;
+    link: string;
+    url: string;
+    title: string;
+    height: number;
+}
+
+interface Copyrigh {
+    provider: Object[];
+    link: string;
+    title: string;
+    image: Image;
+}
+
+interface Location {
+    city: string;
+    area: string;
+    prefecture: string;
+}
+
+interface WeatherNewsResponse {
+    pinpointLocations: PinpointLocations[];
+    link: string;
+    forecasts: Forecast[];
+    location: Location;
+    publicTime: string;
+    copyright: string;
+    title: string;
+    description: Description;
+}
