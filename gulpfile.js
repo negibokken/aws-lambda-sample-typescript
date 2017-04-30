7c307334c520632db2807ca83de6ce7e84b534f0
negibokken <negibokken@gmail.com>
2017-04-30 16:31:47 +0900

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 
 gulp.task('default', () => {
+  'use strict';
   let tsProject = ts.createProject('tsconfig.json');
   const tsResult = gulp.src(['**/*.ts', '!./node_modules/**'])
     .pipe(tsProject())
